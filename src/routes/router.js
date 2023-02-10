869565dd5f20648c2105fc65bdf25b5207e735d6
cesar Santillana <icetom6@hotmail.com>
2023-02-09 19:18:12 -0500

    Feature -> Add Component suscriptionCard, suscriptionPrice and suscription page, adjust route

--- src/routes/router.js
+++ src/routes/router.js
@@ -4,6 +4,7 @@ import Header from '../components/header/header';
 import Footer from '../components/footer/footer';
 import Home from '../pages/home';
 import Categories from '../pages/categories';
+import Suscription from '../pages/suscription';
 
 function Router() {
 
@@ -18,6 +19,7 @@ function Router() {
                 <Route path="science" element={<Categories cat={"science"}/>} />
                 <Route path="technology" element={<Categories cat={"technology"}/>} />
                 <Route path="health" element={<Categories cat={"health"}/>} />
+                <Route path="suscription" element={<Suscription/>} />
             </Routes>
             <Footer />
         </BrowserRouter>
